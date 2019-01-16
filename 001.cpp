597ddebf1f3f30e8fe377351bae4a420b4edad3c
Adam Tekle <tekleadm@gmail.com>
2019-01-16 12:01:23 -0600

    Create 001.cpp

---
+++ 001.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+  int result = 0;
+
+  for (int i = 1; i < 1000; i++)
+    if (i % 3 == 0 || i % 5 == 0)
+      result += i;
+
+  cout << result << endl;
+
+  return 0;
+}
